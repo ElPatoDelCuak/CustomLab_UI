8ea992d3dc68ef4db9ea4a77972b8065a0e62bab
ElPatoDelCuak <davidjuncosaalegre@gmail.com>
2026-04-07 20:19:51 +0200

    add title

--- customlab/src/app/auth/layout.tsx
+++ customlab/src/app/auth/layout.tsx
@@ -1,3 +1,10 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "CustomLab - Login",
+  description: "Login a tu cuenta de customlab"
+};
+
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
 	return children;
 }
